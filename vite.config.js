6330f749b950bdba84869405aacfac2835f9713b
angelmrt <amartinr03@gmail.com>
2024-12-09 13:11:59 +0100

    v3.2 ajustar tamaño del card about_me

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./src", // Cambia la raíz del proyecto a "src"
+  root: "./src", 
   build: {
-    outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
+    outDir: "../docs", 
   },
 });
